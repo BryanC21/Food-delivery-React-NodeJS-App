78063169a6b3585a82f04f59a7e6a199a7e3c25a
Bryan Caldera <bryanicaldera@gmail.com>
2021-02-17 18:39:47 -0800

    linked about pages

--- application/front-end/my-app/src/App.js
+++ application/front-end/my-app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import "./css/index.css";
+import Navbar from "./utility/Navbar/Navbar";
+import Footer from "./utility/Footer/Footer";
+import Home from "./pages/Home/Home";
+import BC from "./pages/AboutTeam/BryanCaldera";
+import CT from "./pages/AboutTeam/CalvinTan";
+import DF from "./pages/AboutTeam/DennyFeng";
+import HN from "./pages/AboutTeam/HuanNguyen";
+import JT from "./pages/AboutTeam/JohnTo";
+import MM from "./pages/AboutTeam/MarcoMarino";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <div className='App-content'>
+          <Route path='/' component={Navbar} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about/BryanCaldera' component={BC} />
+            <Route exact path='/about/CalvinTan' component={CT} />
+            <Route exact path='/about/DennyFeng' component={DF} />
+            <Route exact path='/about/HuanNguyen' component={HN} />
+            <Route exact path='/about/JohnTo' component={JT} />
+            <Route exact path='/about/MarcoMarino' component={MM} />
+          </Switch>
+        </div>
+        <Route path='/' component={Footer} />
+      </div>
+    </Router>
   );
 }
 
---
+++ application/front-end/my-app/src/pages/AboutTeam/BryanCaldera.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BryanCaldera() {
+  return (
+    <div>
+      <h1>Bryan Caldera</h1>
+      <h2>Team Lead</h2>
+      <p>Computer Science Senior on my last semester at SF State.</p>
+      <p>Hope to develop apps and websites after graduation.</p>
+    </div>
+  );
+}
+export default BryanCaldera;
