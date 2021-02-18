3a66224abb3faf9e707ba89b218b9657d1928610
FengDenny <fengdennyy@gmail.com>
2021-02-18 13:47:30 -0800

    Update DennyFeng.js

--- application/front-end/my-app/src/pages/AboutTeam/DennyFeng.js
+++ application/front-end/my-app/src/pages/AboutTeam/DennyFeng.js
@@ -1,17 +1,22 @@
 import React from "react";
+import pic from "../../images/DF_1.JPG";
 function DennyFeng() {
   return (
-    <div>
-      <header>
-        <h1>Denny Feng</h1>
-        <p>Back-End-lead of Team5</p>
-      </header>
-      <section>
-        <p>
-          Hello, I'm a Computer Science Student, with a passion for MERN stack
-          development and design.I have built so far comes from first designing
-          then developing the design.
+    <div className='individual-about container'>
+      <header className='about-header'>
+        <h1 className='primary-color'>Denny Feng</h1>
+        <p className='secondary-heading secondary-color font-xsm margin-top-10 '>
+          Lead Backend of Team5
         </p>
+      </header>
+      <section className='individual-about-section about-wrapper-two'>
+        <p className='secondary-heading secondary-color width margin-top-10'>
+          Hello, I'm a Computer Science Student, with a passion for MERN stack
+          development and design. My motto is to design first then turn my
+          designs into working applications. Forgot pre-build templates, and
+          find yourself a DevSigner!
+        </p>
+        <img src={pic} alt='Denny Feng' />
       </section>
     </div>
   );
