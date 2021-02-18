79c7c33d265f4e66d75a7386d6ccf6f8835b5765
FengDenny <fengdennyy@gmail.com>
2021-02-18 13:47:26 -0800

    Removed/ reposition AboutTeam button

--- application/front-end/my-app/src/utility/Navbar/Navbar.js
+++ application/front-end/my-app/src/utility/Navbar/Navbar.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import T5Logo from "../../images/logos/T5_logo.svg";
-import { smoothScrolling } from "../../js/smoothScrolling";
 
 function Navbar() {
   return (
     <nav className='navbar primary-background'>
-      {smoothScrolling()}
       <div className='container'>
         <header className='nav-logo'>
           <img src={T5Logo} alt='team5_logo' />
@@ -21,14 +19,6 @@ function Navbar() {
             >
               Home
             </NavLink>
-          </li>
-          <li className='nav-button flex'>
-            <a
-              className=' white-color primary-background font-sm'
-              href='#about-team'
-            >
-              About Team
-            </a>
           </li>
         </ul>
       </div>
