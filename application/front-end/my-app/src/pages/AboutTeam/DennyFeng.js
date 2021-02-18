4b2371b8d9267829abdcf6ae0bdfc254793a3cc1
FengDenny <fengdennyy@gmail.com>
2021-02-17 18:40:56 -0800

    Update DennyFeng.js

--- application/front-end/my-app/src/pages/AboutTeam/DennyFeng.js
+++ application/front-end/my-app/src/pages/AboutTeam/DennyFeng.js
@@ -6,6 +6,13 @@ function DennyFeng() {
         <h1>Denny Feng</h1>
         <p>Back-End-lead of Team5</p>
       </header>
+      <section>
+        <p>
+          Hello, I'm a Computer Science Student, with a passion for MERN stack
+          development and design.I have built so far comes from first designing
+          then developing the design.
+        </p>
+      </section>
     </div>
   );
 }
