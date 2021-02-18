49701878671a2e80c6cf1b1bafb18d102fabcba8
FengDenny <fengdennyy@gmail.com>
2021-02-17 17:52:34 -0800

    Create DennyFeng.js

---
+++ application/front-end/my-app/src/pages/AboutTeam/DennyFeng.js
@@ -0,0 +1,13 @@
+import React from "react";
+function DennyFeng() {
+  return (
+    <div>
+      <header>
+        <h1>Denny Feng</h1>
+        <p>Back-End-lead of Team5</p>
+      </header>
+    </div>
+  );
+}
+
+export default DennyFeng;
