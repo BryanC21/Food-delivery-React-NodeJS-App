afce124d31336e136c10c04d28cd6ada0585eef3
Bryan Caldera <bryanicaldera@gmail.com>
2021-02-19 09:50:28 -0800

    fixed front end error

--- application/back-end/server/frontend.js
+++ application/back-end/server/frontend.js
@@ -6,10 +6,10 @@ const app = express();
 //To use:
 //npm run build - from my-app folder
 //node frontend.js - from this folder
-app.use(express.static(__dirname+'/../../front-end/my-app/build'))
+app.use(express.static(path.join(__dirname, '../..', 'front-end/my-app/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname+'/../../front-end/my-app/build/index.html')
+  res.sendFile(path.join(__dirname, '../..', 'front-end/my-app/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
