b62a034e04637d21b308947bc793ec3057b20d87
FengDenny <fengdennyy@gmail.com>
2021-04-05 19:25:19 -0700

    Update HP_Links.js

--- application/team5_vp-main/client/src/pages/HP_Links.js
+++ application/team5_vp-main/client/src/pages/HP_Links.js
@@ -5,10 +5,15 @@ function HP_Links() {
   return (
     <div>
       <header>
-        <h1>Leave links to pages you create here so we can reach them easily:</h1>
+        <h1>
+          Leave links to pages you create here so we can reach them easily:
+        </h1>
         <p>You need to add routes in src/index.js for links to work also</p>
-        <Link to="/">Home</Link>
+        <Link to='/'>Home</Link>
       </header>
+      <div>
+        <Link to='/HP/home_page'>HP homepage</Link>
+      </div>
     </div>
   );
 }
