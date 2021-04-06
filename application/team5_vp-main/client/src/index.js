db232167b85dd917a6eb46917aa24636771920e8
FengDenny <fengdennyy@gmail.com>
2021-04-05 19:25:00 -0700

    route

--- application/team5_vp-main/client/src/index.js
+++ application/team5_vp-main/client/src/index.js
@@ -4,14 +4,20 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import HP_Links from "./pages/HP_Links";
+import Home from "./pages/Home/Home";
+import Navbar from "./utility/Navbar/Navbar";
+import Footer from "./utility/Footer/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Route path='/HP' component={Navbar} />
       <Switch>
         <Route exact path='/' component={App} />
         <Route exact path='/HP_Pages' component={HP_Links} />
+        <Route exact path='/HP/home_page' component={Home} />
       </Switch>
+      <Route path='/HP' component={Footer} />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
