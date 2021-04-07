bc5e4a183a666ff4df182317d18189bdd927f7cc
FengDenny <fengdennyy@gmail.com>
2021-04-06 19:10:45 -0700

    Update Footer.js

--- application/team5_vp-main/client/src/utility/Footer/Footer.js
+++ application/team5_vp-main/client/src/utility/Footer/Footer.js
@@ -19,8 +19,24 @@ function Footer() {
               </Link>
             </div>
           </div>
-          <div className='col-sm-6'>
+          <div className='col'>
             <h1 className='h4 primary-color-font '>Cusines</h1>
+            <p className='lead primary-color-font'>Chinese</p>
+            <p className='lead primary-color-font'>Italian</p>
+            <p className='lead primary-color-font'>American</p>
+            <p className='lead primary-color-font'>Korean</p>
+          </div>
+          <div className='col'>
+            <h1 className='h4 primary-color-font '>Places to Eat</h1>
+            <p className='lead primary-color-font'>Chinese Restaurant</p>
+            <p className='lead primary-color-font'>Italian Restaurant</p>
+            <p className='lead primary-color-font'>American Restaurant</p>
+            <p className='lead primary-color-font'>Korean Restaurant</p>
+          </div>
+          <div className='col'>
+            <h1 className='h4 primary-color-font '>Company</h1>
+            <p className='lead primary-color-font'>About Us</p>
+            <p className='lead primary-color-font'>Careers</p>
           </div>
         </div>
       </div>
