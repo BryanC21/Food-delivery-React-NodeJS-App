979641cc3cb5604ca2373b274d8e95d91544b9bc
FengDenny <fengdennyy@gmail.com>
2021-04-08 18:19:11 -0700

    Update index.js

--- application/team5_vp-main/client/src/index.js
+++ application/team5_vp-main/client/src/index.js
@@ -5,11 +5,12 @@ import "./index.css";
 import App from "./App";
 import HP_Links from "./pages/HP_Links";
 import customerViewRestaruantMenu from "./pages/customerViewRestaruantMenu";
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 import rootReducer from "./redux/reducers/rootReducer";
 import Home from "./pages/Home/Home";
+import SearchPage from "./pages/Search/SearchPage";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
 import ResMenuPage from "./pages/ResMenuPage";
@@ -19,17 +20,22 @@ const store = createStore(rootReducer, applyMiddleware(thunk)); //save fake data
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Router>
-      <Route path='/HP' component={Navbar} />
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route exact path='/HP_Pages' component={HP_Links} />
-        <Route exact path='/HP/CustomerViewRestaruantMenu' component={customerViewRestaruantMenu} />
-        <Route exact path='/HP/homepage' component={Home} />
-        <Route exact path='/HP/RestaurantMenu' component={ResMenuPage} />
-      </Switch>
-      <Route path='/HP' component={Footer} />
-    </Router>
+      <Router>
+        <Route path='/HP' component={Navbar} />
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route exact path='/HP_Pages' component={HP_Links} />
+          <Route
+            exact
+            path='/HP/CustomerViewRestaruantMenu'
+            component={customerViewRestaruantMenu}
+          />
+          <Route exact path='/HP/homepage' component={Home} />
+          <Route exact path='/HP/search_result' component={SearchPage} />
+          <Route exact path='/HP/RestaurantMenu' component={ResMenuPage} />
+        </Switch>
+        <Route path='/HP' component={Footer} />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
