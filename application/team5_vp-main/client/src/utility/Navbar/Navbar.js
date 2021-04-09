eb8df4bf37c70cb3403ec0284d708562a8c959a4
FengDenny <fengdennyy@gmail.com>
2021-04-09 00:28:57 -0700

    Navbar refactored

--- application/team5_vp-main/client/src/utility/Navbar/Navbar.js
+++ application/team5_vp-main/client/src/utility/Navbar/Navbar.js
@@ -1,85 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../images/logo_letters.png";
-import axios from "axios";
-import SearchPage from "../../pages/Search/SearchPage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setSearchTerm } from "../../redux/actions/searchActions";
-
 library.add(faSearch, faCartPlus);
 
 function Navbar() {
-  const history = useHistory();
-
   const dispatch = useDispatch();
-
-  const [loadRestaurants, setLoadRestaurants] = useState([]);
-  const [filterRestaurant, setFilterRestaurant] = useState([]);
   const [search, setSearch] = useState("");
-  const [searchType, setSearchType] = useState("All");
-
-  useEffect(() => {
-    loadAllRestaurants();
-  }, []);
-
-  const loadAllRestaurants = async () => {
-    const url = "/api/v1/restaurants/getAllRestaurants";
-    await axios.get(url).then((response) => {
-      const { restaurants } = response.data;
-      setLoadRestaurants(restaurants);
-    });
-  };
-
-  // using the search button
-  const setFilter = () => {
-    if (searchType === "All") {
-      setFilterRestaurant(
-        loadRestaurants.filter((restaurants) =>
-          restaurants.restaurant_name
-            .toLowerCase()
-            .includes(search.toLowerCase())
-        )
-      );
-    } else {
-      setFilterRestaurant(
-        loadRestaurants.filter(
-          (restaurants) =>
-            restaurants.cuisine_type
-              .toLowerCase()
-              .includes(searchType.toLowerCase()) &&
-            restaurants.restaurant_name
-              .toLowerCase()
-              .includes(search.toLowerCase())
-        )
-      );
-    }
-  };
-  const handleSearches = async (e) => {
-    //e.preventDefault();
-    /*const url = `/api/v1/search/items?search=${search.toLowerCase()}`;
-    await axios
-      .get(url)
-      .then((response) => {
-        setFilter(response.data);
-        console.log(response.data);
-        history.push("/HP/search_result");
-        filterRestaurant.map((restaurant, id) => (
-          <SearchPage key={id} {...restaurant} />
-        ));
-      })
-      .catch((error) => {
-        console.log(error);
-      });*/
-  };
-
-  const handleSelector = (event) => {
-    // console.log(event.target.value)
-    setSearchType(event.target.value);
-  };
 
   return (
     <div className='navbar navbar-light bg-light '>
@@ -91,7 +22,7 @@ function Navbar() {
           <div className='dropdown me-3'>
             <select
               className='btn btn-secondary dropdown-toggle'
-              onChange={handleSelector}
+              onChange={(e) => dispatch(setSearchTerm(e.target.value))}
             >
               <optgroup label='Cuisine'>
                 <option value='All'>All</option>
@@ -109,7 +40,7 @@ function Navbar() {
             aria-label='Search'
             onChange={(e) => setSearch(e.target.value)}
           />
-          <Link to={{ pathname: "/HP/RestaurantMenu", param1: search }}>
+          <Link to={{ pathname: "/HP/search_result_menu", param1: search }}>
             <button
               className='btn btn-search btn-outline-success'
               type='submit'
