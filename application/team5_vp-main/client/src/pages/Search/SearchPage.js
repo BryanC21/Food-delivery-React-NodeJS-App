fe1e50b788f6cf9da6c41d61c52df65eeb67dea7
FengDenny <fengdennyy@gmail.com>
2021-04-09 00:27:58 -0700

    Update SearchPage.js

--- application/team5_vp-main/client/src/pages/Search/SearchPage.js
+++ application/team5_vp-main/client/src/pages/Search/SearchPage.js
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const SearchPage = (props) => {
   const { restaurant_name, restaurant_logo, address } = props;
 
   return (
-    <div className='container'>
-      <div className='col-sm-6'>
-        <div className='card mb-4 shadow-sm'>
-          <img
-            src={restaurant_logo}
-            className='card-img-top card-img'
-            alt='logo'
-          />
-          <div className='card-body card-img'>
-            <h5 className='card-title'>{restaurant_name}</h5>
-            <p className='card-text'>{address}</p>
-          </div>
+    <div className='col-sm-6'>
+      <div className='card mb-4 shadow-sm'>
+        <img
+          src={restaurant_logo}
+          className='card-img-top card-img'
+          alt='logo'
+        />
+        <div className='card-body card-img'>
+          <h5 className='card-title'>{restaurant_name}</h5>
+          <p className='card-text'>{address}</p>
         </div>
       </div>
     </div>
