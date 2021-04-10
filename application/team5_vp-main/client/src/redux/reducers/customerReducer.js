13837d770bc5ef8e5329a6a5cf92c88e48a58dba
l90320825 <johnto1998@hotmail.com>
2021-04-10 03:24:54 -0700

    basic cart page, customerAction

--- application/team5_vp-main/client/src/redux/reducers/customerReducer.js
+++ application/team5_vp-main/client/src/redux/reducers/customerReducer.js
@@ -1,18 +1,24 @@
 const INITIAL_STATE = {
-    restaruant_menu: [{name: "burger", description: "Taste good", price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"},{name: "burger", description: "Taste good",price: "17.99"}],
-    search_type: ""
+    restaruant_menu: [{ name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }, { name: "burger", description: "Taste good", price: "17.99" }],
+    search_type: "",
+    cart: []
 };
 
 const customerReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case "SET_RESTARUANT_MENU":
             return {
-            ...state,
-            restaruant_menu: action.restaruant_menu,
+                ...state,
+                restaruant_menu: action.restaruant_menu,
+            };
+        case "SET_CART":
+            return {
+                ...state,
+                cart: action.cart,
             };
         default:
             return state;
     }
-    };
+};
 
 export default customerReducer;
