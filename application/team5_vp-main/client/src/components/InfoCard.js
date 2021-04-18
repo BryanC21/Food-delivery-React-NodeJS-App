46bd588a29906a92f10afebcf4a1b6c4ca2eb0ae
Calvin Tan <ctan4@mail.sfsu.edu>
2021-04-18 00:45:18 -0700

    InfoCard component added, added some cards to DelivererMM

---
+++ application/team5_vp-main/client/src/components/InfoCard.js
@@ -0,0 +1,29 @@
+import React from "react"
+import MapContainer from "./MapContainer"
+import "../infoCard.css"
+
+function InfoCard (props){
+    return(
+        <div className = "infoCard">
+            {/*the style = display:props.etc makes sure the item only renders the element if the corresponding prop exists */}
+            <img className = "imageContainer" src = {props.img}></img>
+
+            <div style ={{padding:"10px"}}>
+                <p style = {{display: props.restaurantName ? "block":"none"}}>Restaurant Name/Address: {props.restaurantName}</p>
+                <p>Food Name: {props.foodName} </p>
+                <p>Order Number:{props.orderNumber}</p>
+                <p style = {{display: props.orderersInfo ? "block":"none"}}>Orderer's Info: {props.orderersInfo}</p>
+                <p style = {{display: props.deliveryTime ? "block":"none"}}>Delivery Time: {props.deliveryTime}</p>
+                <p style = {{display: props.deliveredTime ? "block":"none"}}>Delivered Time: {props.deliveredTime}</p>
+                <p style = {{display: props.deliveredDate ? "block":"none"}}>Delivered Date:{props.deliveredDate}</p>
+                <p style = {{display: props.deliveryDate ? "block":"none"}}>Delivery Date: {props.deliveryDate}</p>
+                <p style = {{display: props.deliverersName ? "block":"none"}}>Deliverer's Name: {props.deliverersName}</p>
+                <p>Status Fulfilled: {props.statusFulfilled}</p>
+                <p style = {{display: props.price ? "block":"none"}}>Price: {props.price}</p>
+                <p style = {{display: props.specialInstructions ? "block":"none"}}>Special Instructions: {props.specialInstructions}</p>
+                </div>
+        </div>
+    )
+}
+
+export default InfoCard
--- application/team5_vp-main/client/src/pages/DelivererMainMenu.js
+++ application/team5_vp-main/client/src/pages/DelivererMainMenu.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./DelivererMainMenu.css";
+import InfoCard from "../components/InfoCard"
+import MapContainer from '../components/MapContainer';
+import { left } from '@popperjs/core';
 
 
-const DelivererMainMenu = ({deliverer}) => {
+const DelivererMainMenu = (props) => {
+    
     return (
-        <div className = 'heading'>
+        <div>
+            <div className = 'heading'>
             <h1 >
-                Deliverer Name
+                {props.name}
+                Placeholder Name
             </h1>
 
             <h6>
-                Number of Orders Completed:
+                Number of Orders Completed: {props.numberOfOrders}
             </h6>
 
         <div>
@@ -27,7 +33,36 @@ const DelivererMainMenu = ({deliverer}) => {
                 My Performance
             </button>
         </div>
-            
+        </div>
+        <div>
+            <h1 className = 'orderHeading'>
+                Completed Orders
+                <div className = 'orderSection'>
+                <InfoCard 
+                img = "https://prods3.imgix.net/images/articles/2017_05/Facebook-hawaiian-pizza-origins.jpg" 
+                restaurantName = "Circular Table" 
+                foodName = "Italian Crucifixion"
+                orderNumber = "2"
+                deliveredTime = "10:00PM"
+                deliveredDate = "4/1/2021"
+                statusFulfilled = "Delivered"
+                ></InfoCard>
+                <InfoCard 
+                img = "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg"
+                restaurantName = "Inside and Outside"
+                foodName = "Daily Calorie Intake Special"
+                orderNumber = "10"
+                deliveredTime = "9:00AM"
+                deliveredDate = "3/21/2019"
+                statusFulfilled = "Delivered"
+                ></InfoCard>
+                
+                
+                
+                </div>
+                
+            </h1>
+        </div>
         </div>
     );
 }
