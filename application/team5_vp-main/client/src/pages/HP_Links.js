66fb5fcd2bd01070f2e757940fec8105888d9943
l90320825 <johnto1998@hotmail.com>
2021-04-21 17:21:16 -0700

    formatted hp_link page

--- application/team5_vp-main/client/src/pages/HP_Links.js
+++ application/team5_vp-main/client/src/pages/HP_Links.js
@@ -11,39 +11,42 @@ function HP_Links() {
         <p>You need to add routes in src/index.js for links to work also</p>
         <Link to='/'>Home</Link>
         <br></br>
-        <Link to='/HP/RestaurantMenu'>RestaurantMenu</Link>
-        <br></br>
+        <div>
+        <Link to='/HP/homepage'>HP homepage</Link>
+      </div>
+        <h2>Customer</h2>
         <Link to='/HP/CustomerViewRestaruantMenu'>
-          CustomerViewRestaruantMenu
+        CustomerViewRestaruantMenu
         </Link>
-        <br></br>
         <br></br>
         <Link to='/HP/customerCart'>
-          CustomerCart
+        CustomerCart
         </Link>
         <br></br>
-        <Link to='/HP/DelivererMainMenu'>DelivererMainMenu</Link>
+        <Link to='/HP/UserRegistration'>UserRegistration</Link>
         <br></br>
+        <h2>Restaurant</h2>
         <Link to='/HP/RestaurantOrderPage'>RestaurantOrderPage</Link>
-        <br></br>
-        <Link to='/HP/DeliveryOrderPage'>DeliveryOrderPage</Link>
-        <br></br>
-        <Link to='/HP/DeliveryOrderDetail'>DeliveryOrderDetail</Link>
         <br></br>
         <Link to='/HP/RestaurantDeliveryOrderDetail'>Restaurant Delivery Order Detail</Link>
         <br></br>
         <Link to='/HP/RestaurantPickupOrderDetails'>Restaurant Pickup Order Details</Link>
         <br></br>
-        <Link to='/HP/UserRegistration'>UserRegistration</Link>
-        <br></br>
         <Link to='/HP/RestaurantRegistration'>RestaurantRegistration</Link>
+        <br></br>
+        <Link to='/HP/RestaurantMenu'>RestaurantMenu</Link>
+        <br></br>
+        <h2>Delivery</h2>
+        <Link to='/HP/DelivererMainMenu'>DelivererMainMenu</Link>
+        <br></br>
+        <Link to='/HP/DeliveryOrderPage'>DeliveryOrderPage</Link>
+        <br></br>
+        <Link to='/HP/DeliveryOrderDetail'>DeliveryOrderDetail</Link>
         <br></br>
         <Link to='/HP/DeliveryRegistration'>DeliveryRegistration</Link>
         <br></br>
       </header>
-      <div>
-        <Link to='/HP/homepage'>HP homepage</Link>
-      </div>
+     
     </div>
   );
 }
