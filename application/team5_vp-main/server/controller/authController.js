4a38ec5ebfe078de53923e6385acf76ea832527b
FengDenny <fengdennyy@gmail.com>
2021-04-23 13:34:16 -0700

    Approved user registration api is tested and good to go

--- application/team5_vp-main/server/controller/authController.js
+++ application/team5_vp-main/server/controller/authController.js
@@ -55,7 +55,7 @@ exports.restaurantRegister = CatchAsync(async (req, res, next) => {
       } else {
         res.status(500).json({
           status: "fail",
-          error: `"Server error, user could not be created"`,
+          error: `"Server error, restaurant owner user could not be created"`,
         });
       }
     })
@@ -86,6 +86,64 @@ exports.delivererRegister = CatchAsync(async (req, res, next) => {
       let baseSQL =
         "INSERT INTO deliverers (name,email,password) VALUES (?,?,?)";
       return db.execute(baseSQL, [name, email, hashedPassword]);
+    })
+    .then(([results, fields]) => {
+      if (results && results.affectedRows) {
+        res.status(200).json({
+          status: "success",
+          message: `${email} has successfully signed up!`,
+        });
+      } else {
+        res.status(500).json({
+          status: "fail",
+          error: `"Server error, deliverer user could not be created"`,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+exports.approvedUserRegister = CatchAsync(async (req, res, next) => {
+  const { username, email, password, address, phone_number } = req.body;
+
+  db.execute("SELECT * FROM approved_users WHERE username=?", [username])
+    .then(([results, fields]) => {
+      if (results && results.length == 0) {
+        return db.execute("SELECT * FROM approved_users WHERE email=?", [
+          email,
+        ]);
+      } else {
+        return next(
+          new AppError(
+            `${username} is associated with an existing account. Please login.`,
+            401
+          )
+        );
+      }
+    })
+    .then(([results, fields]) => {
+      //   If username/email dne, then we hash password and store it in db
+      if (results && results.length == 0) {
+        return bcrypt.hash(password, 15);
+      } else {
+        res.status(200).json({
+          status: "fail",
+          error: `${email} already exist. Please login.`,
+        });
+      }
+    })
+    .then((hashedPassword) => {
+      let baseSQL =
+        "INSERT INTO approved_users (username,email,password, address,phone_number) VALUES (?,?,?,?,?)";
+      return db.execute(baseSQL, [
+        username,
+        email,
+        hashedPassword,
+        address,
+        phone_number,
+      ]);
     })
     .then(([results, fields]) => {
       if (results && results.affectedRows) {
