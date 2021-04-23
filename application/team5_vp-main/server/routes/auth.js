67ff8691915ff562f5d6b4bf1a731041a52513f2
FengDenny <fengdennyy@gmail.com>
2021-04-23 13:23:40 -0700

    Deliverer route initialized

--- application/team5_vp-main/server/routes/auth.js
+++ application/team5_vp-main/server/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { restaurantRegister } = require("../controller/authController");
+const {
+  restaurantRegister,
+  delivererRegister,
+} = require("../controller/authController");
 
 router.post("/registerRestaurant", restaurantRegister);
+router.post("/registerDeliverer", delivererRegister);
 
 module.exports = router;
