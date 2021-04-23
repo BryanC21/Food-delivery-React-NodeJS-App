ea339e9cdee854701ce0e257f27fb71bdeca0c8b
l90320825 <johnto1998@hotmail.com>
2021-04-23 02:48:48 -0700

    CustomerSignIn.js with Register.css, follow Registration style

--- application/team5_vp-main/client/src/pages/CustomerSignIn.js
+++ application/team5_vp-main/client/src/pages/CustomerSignIn.js
@@ -4,9 +4,73 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import "./Register.css";
 
 
 const CustomerSignIn = () => {
+    
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    
+  
+    const handleSubmit = (event) => {
+      console.log(`
+       
+        Email: ${email}
+        Password: ${password}
+       
+      `);
+  
+      event.preventDefault();
+    };
+    return (
+        // Need to start with a div to style more efficiently
+        <div className='auth-form'>
+          {/* If no bootstrap container form will be all the way to the left  */}
+          <form className='formClass container' onSubmit={handleSubmit}>
+            <h1 className='formH1Class'>Sign In </h1>
+    
+         
+    
+            <label className='labelClass'>
+              Please enter your email:
+              <input
+                className='inputClass'
+                name='email'
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+    
+            <label className='labelClass'>
+              Please enter your password:
+              <input
+                className='inputClass'
+                name='password'
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            {"\n"}
+    
+        
+    
+            <button className='buttonClass'>Sign In</button>
+
+            <h1 className='formH1Class'>Or </h1>
+
+            <button className='buttonClass'>Restaurant Sign In</button>
+            <button className='buttonClass'>Delivery Sign In</button>
+          </form>
+        </div>
+
+);
+
+    /*
     return (
         <div>
             <div className='form'>
@@ -45,7 +109,7 @@ const CustomerSignIn = () => {
 
 
 
-    );
+    );*/
 }
 
 export default CustomerSignIn
