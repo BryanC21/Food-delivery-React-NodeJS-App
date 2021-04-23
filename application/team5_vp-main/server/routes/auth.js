8efd1e39d81f0e17be8809b5bcb4b38be78eb134
FengDenny <fengdennyy@gmail.com>
2021-04-23 13:34:39 -0700

    Approved user route initialized

--- application/team5_vp-main/server/routes/auth.js
+++ application/team5_vp-main/server/routes/auth.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   restaurantRegister,
   delivererRegister,
+  approvedUserRegister,
 } = require("../controller/authController");
 
 router.post("/registerRestaurant", restaurantRegister);
 router.post("/registerDeliverer", delivererRegister);
+router.post("/registerApprovedUser", approvedUserRegister);
 
 module.exports = router;
