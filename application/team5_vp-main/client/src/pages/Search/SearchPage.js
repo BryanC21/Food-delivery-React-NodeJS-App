fef791e8c1f3bb86c222d010b44e12a40a0de137
FengDenny <fengdennyy@gmail.com>
2021-04-22 19:55:02 -0700

    search page now has access to address, dollar_sign props

--- application/team5_vp-main/client/src/pages/Search/SearchPage.js
+++ application/team5_vp-main/client/src/pages/Search/SearchPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faMapMarkerAlt);
 
 const SearchPage = (props) => {
-  const { restaurant_name, restaurant_logo, address } = props;
+  const {
+    restaurant_name,
+    restaurant_logo,
+    cuisine_type,
+    address,
+    dollar_sign,
+  } = props;
 
   return (
     <div className='col-sm-6'>
@@ -13,7 +23,17 @@ const SearchPage = (props) => {
         />
         <div className='card-body card-img'>
           <h5 className='card-title'>{restaurant_name}</h5>
-          <p className='card-text'>{address}</p>
+          <h6>
+            {dollar_sign} - {cuisine_type}
+          </h6>
+          <p className='card-text'>
+            <FontAwesomeIcon
+              className='primary-color-font'
+              icon={faMapMarkerAlt}
+              size='1x'
+            />
+            <span className='mx-2'>{address}</span>
+          </p>
         </div>
       </div>
     </div>
