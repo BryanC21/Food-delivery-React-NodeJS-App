7e98124adcacce91992968a78454732ac64448e3
l90320825 <johnto1998@hotmail.com>
2021-04-23 18:03:25 -0700

    customerRestaranutMenu add button and resize card item

--- application/team5_vp-main/client/src/pages/customerViewRestaruantMenu.js
+++ application/team5_vp-main/client/src/pages/customerViewRestaruantMenu.js
@@ -31,21 +31,45 @@ const CustomerViewRestaruantMenu = ({dispatch, restaruant_menu }) => {
                      
 
 
-        <h2 className='head'>Resturant Name</h2>
+        <h2 className='customer-head'>Resturant Name</h2>
 
       </div>
 
       <div className='head-black '>
         <h3> Our Menu </h3>
       </div>
-      <div className="sidenav">
+
+
+      <section className="order-section">
+                <div className="order-content">
+                    <div className="wrapper2">
+                        {restaruant_menu.map((restaruant_menu) =>
+                        <button  className="customer-buttom" onClick={() => {dispatch(setCart(restaruant_menu));  setModalIsOpen(true);}}>
+                            <div className="card card-width">
+                                <img className="card-img-top" src="https://res.cloudinary.com/dis7ep3yq/image/upload/v1616095822/American_hef5n1.jpg" alt="burger"></img>
+                                <div className="customer-card-body">
+                                    <h5 className="card-title">{restaruant_menu.name}</h5>
+                                    <h6 className="card-title">{restaruant_menu.description}</h6>
+                                  
+                                   {/* <button className="bottun " onClick={() => {dispatch(setCart(restaruant_menu));  setModalIsOpen(true);}}><p className="text-color">Add</p></button>*/}
+                                </div>
+                            </div>
+                            </button>
+                        )}
+                    </div>
+                </div>
+
+            </section>
+
+
+    {/*  <div className="sidenav">
         <p>Search filter</p>
       </div>
-      <div className="mergin">
+      <div className="mergin">*/}
 
 
         {/* <CardDeck style={{display: 'flex', flexDirection: 'row', margin: '5rem', flexWrap: "wrap"}}> */}
-        <CardColumns >
+       {/* <CardColumns >
 
           {restaruant_menu.map((restaruant_menu) =>
             <Card border="dark" style={{ margin: '1rem' }}>
@@ -63,7 +87,7 @@ const CustomerViewRestaruantMenu = ({dispatch, restaruant_menu }) => {
                     <Button variant="primary" onClick={() => {dispatch(setCart(restaruant_menu));  setModalIsOpen(true);}} >Add</Button>
                     
                     {/*} <Link variant="primary" onClick={() => dispatch(setItemID(itemList.product_id))} to={`${match.path}/itemPage`}>Check</Link>*/}
-                  </Card.Body>
+            {/*      </Card.Body>
                 </Col>
               </Row>
 
@@ -71,7 +95,7 @@ const CustomerViewRestaruantMenu = ({dispatch, restaruant_menu }) => {
 
 
           )}
-        </CardColumns>
+        </CardColumns>*/}
         {/*</CardDeck>*/}
 
       </div>
@@ -82,18 +106,10 @@ const CustomerViewRestaruantMenu = ({dispatch, restaruant_menu }) => {
 
 
 
-      {/* {
-              props.itemList.map(item=>
-              <div>
-                 <a href={'http://localhost:3001/api/items?itemId='+item.product_id}> 
-                  {item.product_name}
-                 </a> *
-                {s}is selling for ${item.price}
-              </div>)
-              } */}
+    
 
 
-    </div>
+   
   )
 };
 
