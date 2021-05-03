8d4b3efebbbff7f1702720e772c7b0e44718dbbb
FengDenny <fengdennyy@gmail.com>
2021-05-02 17:55:42 -0700

    Login route has been initialized and ready to be used

--- application/team5_vp-main/server/routes/auth.js
+++ application/team5_vp-main/server/routes/auth.js
@@ -5,10 +5,18 @@ const {
   restaurantRegister,
   delivererRegister,
   approvedUserRegister,
+  restaurantLogin,
+  delivererLogin,
+  approvedUserLogin,
 } = require("../controller/authController");
 
+// Register Route
 router.post("/registerRestaurant", restaurantRegister);
 router.post("/registerDeliverer", delivererRegister);
 router.post("/registerApprovedUser", approvedUserRegister);
 
+// Login Routes
+router.post("/restaurantLogin", restaurantLogin);
+router.post("/delivererLogin", delivererLogin);
+router.post("/userLogin", approvedUserLogin);
 module.exports = router;
