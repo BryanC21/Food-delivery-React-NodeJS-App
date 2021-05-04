f19a50ce755228ba31dc9f11110b446e3a5c4424
l90320825 <johnto1998@hotmail.com>
2021-05-03 23:04:47 -0700

    add redirect to registration

--- application/team5_vp-main/client/src/pages/UserRegistration.js
+++ application/team5_vp-main/client/src/pages/UserRegistration.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../styling/Registration.css";
 import axios from "axios";
+import {Redirect, useHistory } from "react-router-dom"
 
 
 
@@ -12,6 +13,21 @@ export default function UserRegistration() {
   const [address, setAddress] = React.useState("");
   const [phone_number, setPhone_Number] = React.useState("");
   const [acceptedTerms, setAcceptedTerms] = React.useState(false);
+  const [login, setLogin] = React.useState(false);
+
+  
+
+  const handleSubmit2 = () => {
+    
+  
+    console.log("test");
+    setLogin(true);
+   // let history = useHistory;
+    //{useHistory.push("/HP/CustomerViewRestaruantMenu");}
+ 
+     // return (<Redirect to="/HP/CustomerViewRestaruantMenu" />);
+  
+  };
 
   
 
@@ -28,12 +44,17 @@ export default function UserRegistration() {
     try {
       const res = await axios.post("/api/v1/auth/registerApprovedUser", data);
       console.log("RESTAURANT INFORMATION: ", res);
+      setLogin(true);
     } catch (err) {
       console.log(err);
     }
   };
+  if(login) {
+    return <Redirect to="/HP/CustomerViewRestaruantMenu" />;
+  }
 
   return (
+   
     // Need to start with a div to style more efficiently
     <div className='auth-form'>
       {/* If no bootstrap container form will be all the way to the left  */}
@@ -47,10 +68,6 @@ export default function UserRegistration() {
             name='name'
             type='text'
             value={username}
-           /*  onInvalid = {(e) =>e.target.setCustomValidity("Please select a date in the past.")}*/
-            onSubmit = {(e) =>e.target.setCustomValidity("gay")}
-            on
-           /* onChange={(e) =>e.target.setCustomValidity("Please select a date in the past.")}*/
             onChange={(e) => setUsername(e.target.value)}
             required
           />
