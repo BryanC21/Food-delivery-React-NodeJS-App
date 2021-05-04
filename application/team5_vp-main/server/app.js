fc1d6a2cd75e711c8a8fdbb8ec6ad560a848a576
FengDenny <fengdennyy@gmail.com>
2021-05-04 12:02:17 -0700

    Added cookie parser and sessions middleware

--- application/team5_vp-main/server/app.js
+++ application/team5_vp-main/server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+var cookieParser = require("cookie-parser");
 
 const restaurantRoutes = require("./routes/restaurants");
 const authRoutes = require("./routes/auth");
@@ -36,6 +37,7 @@ app.use(
 // Body Parser
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
 
 // for image upload
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -46,6 +48,12 @@ if (process.env.NODE_ENV == "development") {
 } else if (process.env.NODE_ENV == "production") {
   app.use(morgan("production"));
 }
+app.use((req, res, next) => {
+  if (req.session.email) {
+    res.locals.logged = true;
+  }
+  next();
+});
 
 // routes
 app.use("/api/v1/restaurants", restaurantRoutes);
