cb32f9b694ce0264ddcd87c0e372d1c68aec56fb
FengDenny <fengdennyy@gmail.com>
2021-05-05 01:14:42 -0700

    authReducer has been added

--- application/team5_vp-main/client/src/redux/reducers/rootReducer.js
+++ application/team5_vp-main/client/src/redux/reducers/rootReducer.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux';
-import customerReducer from './customerReducer';
-import searchReducer from './searchReducer'
-
+import { combineReducers } from "redux";
+import customerReducer from "./customerReducer";
+import searchReducer from "./searchReducer";
+import authReducer from "./authReducer";
 export default combineReducers({
-    customerReducer,
-    searchReducer,
+  customerReducer,
+  searchReducer,
+  auth: authReducer,
 });
