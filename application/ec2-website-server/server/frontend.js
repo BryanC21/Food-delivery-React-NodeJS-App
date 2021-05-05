351fb95204943a34058387f14977b815fff18093
Bryan Caldera <bryanicaldera@gmail.com>
2021-05-05 16:03:09 -0700

    added note on restaurantMenu and fix frontend link

--- application/ec2-website-server/server/frontend.js
+++ application/ec2-website-server/server/frontend.js
@@ -7,11 +7,11 @@ const app = express();
 //npm run build - from my-app folder
 //node frontend.js - from this folder
 //app.use(express.static(path.join(__dirname, '../..', 'front-end/my-app/build')));
-app.use(express.static(path.join(__dirname, '../..', 'team5_vp-main/client/build')));
+app.use(express.static(path.join(__dirname, '../..', 'website-code/client/build')));
 
 app.get('*', (req, res) => {
   //res.sendFile(path.join(__dirname, '../..', 'front-end/my-app/build', 'index.html'));
-  res.sendFile(path.join(__dirname, '../..', 'team5_vp-main/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../..', 'website-code/client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
