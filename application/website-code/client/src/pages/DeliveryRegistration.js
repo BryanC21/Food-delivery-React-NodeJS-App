4dd8adcbaa61ff507acef06f30b6ea50eaa607f7
Calvin Tan <calvintan607@gmail.com>
2021-05-21 16:30:19 -0700

    loader added to all registration pages

--- application/website-code/client/src/pages/DeliveryRegistration.js
+++ application/website-code/client/src/pages/DeliveryRegistration.js
@@ -2,17 +2,32 @@ import React, { Component } from "react";
 import "../styling/Registration.css";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { css } from "@emotion/react";
+import BounceLoader from "react-spinners/BounceLoader";
+
+//loader css
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -50px;
+`;
 
 export default function DeliveryRegistration() {
   const [username, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [acceptedTerms, setAcceptedTerms] = React.useState(false);
-
+  const [loading,setLoading] = React.useState(false);
   const history = useHistory();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true)
     const data = {
       username,
       email,
@@ -85,6 +100,15 @@ export default function DeliveryRegistration() {
 
         <button className='buttonClass'>Register</button>
       </form>
+      {
+       /*loader component below
+       */
+       }
+
+      <div className="sweet-loading">
+        <BounceLoader color={"#966CA2"} loading={loading} css={override} size={100} />
+      </div>
+    
     </div>
   );
 }
